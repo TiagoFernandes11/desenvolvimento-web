3fa34e1214559f81904524187a5cc34c5c675aa7
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-03-23 11:48:23 -0300

    Section 11 revisão

---
+++ section11-ESNext/revisão.js
@@ -0,0 +1,37 @@
+//Novas funções
+const obj = { a: 1, b: 2, c: 3 };
+
+console.log(Object.values(obj));
+console.log(Object.keys(obj));
+console.log(Object.entries(obj));
+
+//Melhorias nas notações literais
+const nome = "Tiago";
+
+const pessoa = {
+  nome,
+  ola() {
+    return `${this.nome} diz Olá!`;
+  },
+};
+
+console.log(pessoa.ola());
+
+//Class
+
+class Animal {
+  constructor() {
+    this.comer = () => console.log("Comendo");
+  }
+}
+
+class Cachorro extends Animal {
+  falar() {
+    console.log("AU AU");
+  }
+}
+
+let dog = new Cachorro();
+
+dog.comer();
+dog.falar();
