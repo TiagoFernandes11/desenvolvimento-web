f6f48349fce9088354bf0c8d44cf50688be33ff7
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-03-24 14:02:19 -0300

    Ajax2

--- section16-AJAX/server.js
+++ section16-AJAX/server.js
@@ -1,5 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "./upload");
+  },
+  filename: function (req, file, callback) {
+    callback(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage }).single("arquivo");
 
 const app = express();
 
@@ -7,6 +19,15 @@ app.use(express.static("."));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/teste", (req, res) => res.send("<h1>Ok</h1>"))
+app.get("/teste", (req, res) => res.send("<h1>Ok</h1>"));
 
-app.listen(3000, () => console.log("Server funcionando na porta 3000"))
+app.post("/upload", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return res.end("Ocorreu um erro.");
+    }
+    res.end("Concluido com sucesso");
+  });
+});
+
+app.listen(3000, () => console.log("Server funcionando na porta 3000"));
