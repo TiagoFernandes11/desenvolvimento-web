9027ea4439d2ec4d7e1fcee5c13c9634b98b0317
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-03-29 11:31:39 -0300

    React Múltiplos Componentes

--- section22-React/exercicios/src/index.js
+++ section22-React/exercicios/src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from "react-dom";
 
 import Primeiro from "./componentes/Primeiro";
 import BomDia from "./componentes/BomDia";
+import Multiplos from "./componentes/Multiplos";
 
+ReactDOM.render(<BomDia nome="Tiago" />, document.getElementById("root"));
+ReactDOM.render(<Primeiro />, document.getElementById("primeiro"));
 
-ReactDOM.render(<BomDia nome="Tiago"/>, document.getElementById('root'));
-ReactDOM.render(<Primeiro/>, document.getElementById("primeiro"))
+ReactDOM.render(
+  <div>
+    <Multiplos.BoaTarde nome="Rita" />
+    <Multiplos.BoaNoite nome="Cassia" />
+  </div>,
+  document.getElementById("multiplos")
+);
