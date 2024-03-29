76705ddf6899eea5b3ffe07ab92b3c0aeee462fd
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-03-29 11:58:52 -0300

    React Componentes de classe

--- section22-React/exercicios/src/index.js
+++ section22-React/exercicios/src/index.js
@@ -5,13 +5,17 @@ import Primeiro from "./componentes/Primeiro";
 import BomDia from "./componentes/BomDia";
 import Multiplos from "./componentes/Multiplos";
 
-ReactDOM.render(<BomDia nome="Tiago" />, document.getElementById("root"));
-ReactDOM.render(<Primeiro />, document.getElementById("primeiro"));
+import Saudacao from "./componentes/SaudacaoClasse";
 
 ReactDOM.render(
   <div>
+    <BomDia nome="Tiago" />
+    <Primeiro />
     <Multiplos.BoaTarde nome="Rita" />
     <Multiplos.BoaNoite nome="Cassia" />
+    <hr></hr>
+    <p>Componentes de classe</p>
+    <Saudacao tipo="Bom dia" nome="Tiago"/>
   </div>,
-  document.getElementById("multiplos")
+  document.getElementById("root")
 );
